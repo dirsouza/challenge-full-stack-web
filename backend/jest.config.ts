83a3882d4efd8f3fab4469506c48eb2e380d455b
Diogo Souza <diogo.souza@uds.com.br>
2020-11-21 17:59:01 -0400

    refatorado config do jest

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,23 +1,28 @@
-import { pathsToModuleNameMapper } from 'ts-jest/utils';
-const { compilerOptions } = require('./tsconfig.json');
-
 export default {
   clearMocks: true,
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
+  rootDir: './src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    'App/(.*)$': '<rootDir>/app/$1',
+    'Domain/(.*)$': '<rootDir>/domain/$1',
+    'Infrastructure/(.*)$': '<rootDir>/infrastructure/$1',
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: [
-    "js",
-    "json",
-    "ts"
+    'js',
+    'json',
+    'ts',
   ],
-  rootDir: "./src",
-  testRegex: ".*\\.spec\\.ts$",
-  transform: {
-    "^.+\\.(t|j)s$": "ts-jest"
-  },
   collectCoverageFrom: [
-    "**/*.(t|j)s"
+    '**/*.(t|j)s',
+    '!<rootDir>/main.(t|j)s',
+    '!<rootDir>/domain/dtos/**/*.(t|j)s',
+    '!<rootDir>/domain/entities/**/*.(t|j)s',
+    '!<rootDir>/infrastructure/**/*.(t|j)s',
   ],
-  coverageDirectory: "coverage"
-}
+  coverageDirectory: '../coverage',
+};
