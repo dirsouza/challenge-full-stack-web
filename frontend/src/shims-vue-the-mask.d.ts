87acb600a73b4e980bcb72fa8a4ea9befe289708
Diogo Souza <diogo.souza@uds.com.br>
2020-11-23 02:17:26 -0400

    adicionado types de novas dependencias

---
+++ frontend/src/shims-vue-the-mask.d.ts
@@ -0,0 +1,10 @@
+declare module 'vue-the-mask' {
+  import VueTheMask, {
+    TheMask, mask, tokens, version,
+  } from 'vue-the-mask';
+
+  export default VueTheMask;
+  export {
+    TheMask, mask, tokens, version,
+  };
+}
