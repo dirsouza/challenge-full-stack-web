7a3f9bce101409a75af736c3fcf567147bcaed57
Diogo Souza <diogo.souza@uds.com.br>
2020-11-23 02:19:47 -0400

    adicionado a importação dos plugins e override de estilos

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import '@/support/plugins/vee-validate';
+import '@/support/plugins/vue-the-mask';
+import '@/support/plugins/vue-loading';
+import '@/support/plugins/sweetalert';
 import App from '@/App.vue';
 import router from '@/router';
 import vuetify from '@/support/plugins/vuetify';
+
+import '@/sass/overrides.scss';
 
 Vue.config.productionTip = false;
 
